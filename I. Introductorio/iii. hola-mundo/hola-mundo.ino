7e1ffef145a75206bfa81a12ea06753c20683e31
Cristian Delgado <cjdg@users.noreply.github.com>
2018-06-07 22:01:28 -0500

    Create hola-mundo.ino

---
+++ I. Introductorio/iii. hola-mundo/hola-mundo.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("Hola ");
+  Serial.print("mundo");
+  Serial.print('\n');
+  delay(2000);
+  Serial.println("Hola Mundo 2");
+}
+
+void loop()
+{
+
+
+}
